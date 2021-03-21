040c62bd05fff9121c4869790d7bbe8da437c1a4
David McKay <david@rawkode.com>
2021-03-21 11:42:21 +0000

    fix: chmod should be integer (#52)

--- src/actions/file/copy.rs
+++ src/actions/file/copy.rs
@@ -23,8 +23,8 @@ fn from_octal<'de, D>(deserializer: D) -> Result<u32, D::Error>
 where
     D: Deserializer<'de>,
 {
-    let s: String = Deserialize::deserialize(deserializer)?;
-    u32::from_str_radix(&s, 8).map_err(D::Error::custom)
+    let chmod: u32 = Deserialize::deserialize(deserializer)?;
+    u32::from_str_radix(&chmod.to_string(), 8).map_err(D::Error::custom)
 }
 
 fn default_chmod() -> u32 {
