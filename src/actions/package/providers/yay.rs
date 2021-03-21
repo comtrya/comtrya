c49bde55828f63b079c3da9df40c76c3d6b5e80d
Lucas Severo Alves <lucassalves65@gmail.com>
2021-03-21 12:43:36 +0100

    Use new command approach

--- src/actions/package/providers/yay.rs
+++ src/actions/package/providers/yay.rs
@@ -1,5 +1,6 @@
 use super::PackageProvider;
 use crate::actions::{package::PackageVariant, ActionError};
+use crate::utils::command::{run_command, Command};
 use serde::{Deserialize, Serialize};
 use std::process::{Command, Output, Stdio};
 use tracing::{debug, info, span, warn};
@@ -27,43 +28,41 @@ impl PackageProvider for Yay {
         let span = span!(tracing::Level::INFO, "bootstrap").entered();
 
         // Install base-devel and git to be able to pull and build/compile stuff
-        Command::new("pacman")
-            .args(&["-S", "--noconfirm", "base-devel", "git"])
-            .stdin(Stdio::inherit())
-            .stdout(Stdio::inherit())
-            .stderr(Stdio::inherit())
-            .output()
-            .unwrap();
+        run_command(Command {
+            name: String::from("pacman"),
+            env: self.env(),
+            args: vec![
+                String::from("-S"),
+                String::from("--noconfirm"),
+                String::from("base-devel"),
+                String::from("git"),
+            ],
+            require_root: true,
+        })?;
 
         // Clone Yay from AUR
-        Command::new("git")
-            .args(&["clone", "https://aur.archlinux.org/yay.git", "/tmp/yay"])
-            .stdin(Stdio::inherit())
-            .stdout(Stdio::inherit())
-            .stderr(Stdio::inherit())
-            .output()
-            .unwrap();
+        run_command(Command {
+            name: String::from("git"),
+            env: self.env(),
+            args: vec![
+                String::from("clone"),
+                String::from("https://aur.archlinux.org/yay.git"),
+                String::from("/tmp/yay"),
+            ],
+            require_root: false,
+        })?;
 
         // Install Yay from PKGBUILD
-        let result = match Command::new("makepkg")
-            .args(&["-si", "--noconfirm"])
-            .current_dir("/tmp/yay")
-            .output()
-        {
-            Ok(Output { status, .. }) if status.success() => Ok(()),
-
-            Ok(Output { stderr, .. }) => Err(ActionError {
-                message: String::from_utf8(stderr).unwrap(),
-            }),
-
-            Err(e) => Err(ActionError {
-                message: e.to_string(),
-            }),
-        };
+        run_command(Command {
+            name: String::from("makepkg"),
+            env: self.env(),
+            args: vec![String::from("-si"), String::from("--noconfirm")],
+            require_root: true,
+        })?;
 
         span.exit();
 
-        result
+        Ok(())
     }
 
     fn has_repository(&self, _package: &PackageVariant) -> bool {
@@ -79,23 +78,17 @@ impl PackageProvider for Yay {
     }
 
     fn install(&self, package: &PackageVariant) -> Result<(), ActionError> {
-        match Command::new("yay")
-            .args(&["-S", "--noconfirm"])
-            .args(package.extra_args.clone())
-            .args(&package.packages())
-            .output()
-        {
-            Ok(_) => {
-                info!(
-                    message = "Package Installed",
-                    packages = package.packages().join(",").as_str()
-                );
+        run_command(Command {
+            name: String::from("yay"),
+            env: self.env(),
+            args: vec![String::from("-S"), String::from("--noconfirm")]
+                .into_iter()
+                .chain(package.extra_args.clone())
+                .chain(package.packages())
+                .collect(),
+            require_root: true,
+        })?;
 
-                Ok(())
-            }
-            Err(error) => Err(ActionError {
-                message: error.to_string(),
-            }),
-        }
+        Ok(())
     }
 }
