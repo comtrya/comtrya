e4aa3e4cb47939c8c1e005e6a03cce551c7811ce
David McKay <david@rawkode.com>
2021-03-31 23:33:02 +0100

    fix: there's no templating for directory actions, so remove tera (#76)
    * fix: there's no templating for directory actions, so remove tera
    
    * fix: use pull_request_target, which allows a write token for labelling issues

--- src/actions/directory/mod.rs
+++ src/actions/directory/mod.rs
@@ -1,31 +1,4 @@
 pub mod copy;
-use super::ActionError;
 use crate::actions::Action;
-use crate::manifests::Manifest;
-use tera::Tera;
-use tracing::trace;
 
-pub trait DirectoryAction: Action {
-    fn init(&self, manifest: &Manifest) -> Tera {
-        let files_directory = manifest.root_dir.clone().unwrap().join("files");
-
-        trace!(
-            message = "Creating Private Tera for File Action",
-            directory = files_directory.join("**/*").to_str().unwrap()
-        );
-
-        match Tera::new(files_directory.join("**/*").to_str().unwrap()) {
-            Ok(t) => t,
-            Err(e) => panic!(
-                "Failed to initialise Tera for {:?}: {:?}",
-                manifest.name.clone().unwrap(),
-                e
-            ),
-        }
-    }
-
-    fn load(&self, manifest: &Manifest, path: &str) -> Result<String, ActionError> {
-        std::fs::read_to_string(manifest.root_dir.clone().unwrap().join("files").join(path))
-            .map_err(ActionError::from)
-    }
-}
+pub trait DirectoryAction: Action {}
