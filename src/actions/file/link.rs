85ac0a8d5cfa22e709d2e634389ba9d644d83344
Brian Ketelsen <bketelsen@gmail.com>
2021-05-24 14:22:00 -0400

    make link more clear, and make it work

--- src/actions/file/link.rs
+++ src/actions/file/link.rs
@@ -5,11 +5,12 @@ use crate::steps::Step;
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
 use tera::Context;
+use tracing::error;
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct FileLink {
-    pub from: String,
-    pub to: String,
+    pub source: String,
+    pub target: String,
 }
 
 impl FileLink {}
@@ -17,13 +18,19 @@ impl FileLink {}
 impl FileAction for FileLink {}
 
 impl Action for FileLink {
-    fn plan(&self, _: &Manifest, _: &Context) -> Vec<Step> {
+    fn plan(&self, manifest: &Manifest, _: &Context) -> Vec<Step> {
         use crate::atoms::command::Exec;
         use crate::atoms::file::Link;
+        let from: PathBuf = match self.resolve(manifest, &self.source) {
+            Ok(from) => from,
+            Err(_) => {
+                error!("Failed to resolve path for file link");
+                return vec![];
+            }
+        };
 
-        let from = PathBuf::from(&self.from);
-        let to = PathBuf::from(&self.to);
-        let parent = from.clone();
+        let to = PathBuf::from(&self.target);
+        let parent = to.clone();
 
         vec![
             Step {
@@ -39,7 +46,10 @@ impl Action for FileLink {
                 finalizers: vec![],
             },
             Step {
-                atom: Box::new(Link { from, to }),
+                atom: Box::new(Link {
+                    source: from,
+                    target: to,
+                }),
                 initializers: vec![],
                 finalizers: vec![],
             },
@@ -55,16 +65,16 @@ mod tests {
     fn it_can_be_deserialized() {
         let yaml = r#"
 - action: file.link
-  from: a
-  to: b
+  source: a
+  target: b
 "#;
 
         let mut actions: Vec<Actions> = serde_yaml::from_str(yaml).unwrap();
 
         match actions.pop() {
             Some(Actions::FileLink(link)) => {
-                assert_eq!("a", link.from);
-                assert_eq!("b", link.to);
+                assert_eq!("a", link.source);
+                assert_eq!("b", link.target);
             }
             _ => {
                 panic!("FileLink didn't deserialize to the correct type");
