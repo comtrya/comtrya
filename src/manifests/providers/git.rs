3aab0699b2352b83430ced5c29cdeeb82ff5271a
David McKay <david@rawkode.com>
2021-06-02 12:34:48 +0100

    feat: support #ref:path with Git sourced manifests

--- src/manifests/providers/git.rs
+++ src/manifests/providers/git.rs
@@ -6,6 +6,13 @@ use tracing::{error, info};
 #[derive(Debug)]
 pub struct GitManifestProvider;
 
+#[derive(Debug, PartialEq)]
+pub(crate) struct GitConfig {
+    repository: String,
+    branch: Option<String>,
+    path: Option<String>,
+}
+
 impl ManifestProvider for GitManifestProvider {
     fn looks_familiar(&self, url: &str) -> bool {
         use regex::Regex;
@@ -15,8 +22,8 @@ impl ManifestProvider for GitManifestProvider {
     }
 
     fn resolve(&self, url: &str) -> Result<std::path::PathBuf, super::ManifestProviderError> {
-        // Extract this to a function!
-        let clean_repo_url = self.clean_git_url(&url);
+        let git_config = self.parse_config_url(url);
+        let clean_repo_url = self.clean_git_url(&git_config.repository);
         let cache_path = dirs_next::cache_dir()
             .unwrap()
             .join("comtrya")
@@ -25,7 +32,8 @@ impl ManifestProvider for GitManifestProvider {
             .join(clean_repo_url);
 
         let git_sync = GitSync {
-            repo: url.to_string(),
+            repo: git_config.repository,
+            branch: git_config.branch,
             dir: cache_path.clone(),
             ..Default::default()
         };
@@ -46,11 +54,43 @@ impl ManifestProvider for GitManifestProvider {
             return Err(ManifestProviderError::NoResolution);
         }
 
-        Ok(cache_path)
+        Ok(cache_path.join(git_config.path.unwrap_or(String::from(""))))
     }
 }
 
 impl GitManifestProvider {
+    fn parse_config_url(&self, uri: &str) -> GitConfig {
+        let (repository, parts) = match uri.split_once("#") {
+            Some(parts) => parts,
+            None => {
+                return GitConfig {
+                    repository: String::from(uri),
+                    branch: None,
+                    path: None,
+                };
+            }
+        };
+
+        let (reference, path) = match parts.split_once(":") {
+            Some(("", path)) => (None, Some(path.to_string())),
+            Some((reference, "")) => (Some(reference.to_string()), None),
+            Some((reference, path)) => (Some(reference.to_string()), Some(path.to_string())),
+            None => {
+                return GitConfig {
+                    repository: String::from(repository),
+                    branch: Some(parts.to_string()),
+                    path: None,
+                }
+            }
+        };
+
+        return GitConfig {
+            repository: String::from(repository),
+            branch: reference,
+            path: path,
+        };
+    }
+
     fn clean_git_url(&self, uri: &str) -> String {
         uri.to_string()
             .replace("https", "")
@@ -65,6 +105,8 @@ impl GitManifestProvider {
 #[cfg(test)]
 #[cfg(unix)]
 mod test {
+    use crate::manifests::providers::git::GitConfig;
+
     use super::super::ManifestProvider;
     use super::GitManifestProvider;
 
@@ -106,6 +148,57 @@ mod test {
         assert_eq!(
             false,
             git_manifest_provider.looks_familiar(&String::from("github.com/comtrya/comtrya"))
+        );
+    }
+
+    #[test]
+    fn test_parse_config_url() {
+        let git_manifest_provider = GitManifestProvider {};
+
+        assert_eq!(
+            GitConfig {
+                repository: String::from("https://hubgit.com/comtrya/comtrya"),
+                branch: None,
+                path: None,
+            },
+            git_manifest_provider.parse_config_url("https://hubgit.com/comtrya/comtrya")
+        );
+
+        assert_eq!(
+            GitConfig {
+                repository: String::from("https://hubgit.com/comtrya/comtrya"),
+                branch: Some(String::from("main")),
+                path: None,
+            },
+            git_manifest_provider.parse_config_url("https://hubgit.com/comtrya/comtrya#main")
+        );
+
+        assert_eq!(
+            GitConfig {
+                repository: String::from("https://hubgit.com/comtrya/comtrya"),
+                branch: Some(String::from("main")),
+                path: Some(String::from("comtrya")),
+            },
+            git_manifest_provider
+                .parse_config_url("https://hubgit.com/comtrya/comtrya#main:comtrya")
+        );
+
+        assert_eq!(
+            GitConfig {
+                repository: String::from("https://hubgit.com/comtrya/comtrya"),
+                branch: Some(String::from("main")),
+                path: None,
+            },
+            git_manifest_provider.parse_config_url("https://hubgit.com/comtrya/comtrya#main:")
+        );
+
+        assert_eq!(
+            GitConfig {
+                repository: String::from("https://hubgit.com/comtrya/comtrya"),
+                branch: None,
+                path: Some(String::from("comtrya")),
+            },
+            git_manifest_provider.parse_config_url("https://hubgit.com/comtrya/comtrya#:comtrya")
         );
     }
 
