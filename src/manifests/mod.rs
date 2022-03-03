25b2e01e995b544b36f0fdff258bbabfbbf7fb3c
David Flanagan <david@rawkode.com>
2022-03-03 21:12:44 +0000

    chore: test get_manifest_name

--- src/manifests/mod.rs
+++ src/manifests/mod.rs
@@ -24,3 +24,142 @@ pub struct Manifest {
     #[serde(skip)]
     pub dag_index: Option<NodeIndex<u32>>,
 }
+
+pub fn get_manifest_name(manifest_directory: &PathBuf, location: &PathBuf) -> String {
+    let local_name = location.strip_prefix(&manifest_directory).unwrap();
+    let manifest_name =
+        local_name
+            .components()
+            .into_iter()
+            .fold(String::from(""), |mut s, next| {
+                if !s.is_empty() {
+                    s.push('.');
+                }
+                s.push_str(next.as_os_str().to_str().unwrap());
+
+                s
+            });
+
+    let manifest_name = manifest_name.trim_end_matches(".yaml");
+    let manifest_name = manifest_name.trim_end_matches(".yml");
+
+    String::from(manifest_name.trim_end_matches(".main"))
+}
+
+#[cfg(test)]
+#[cfg(unix)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_top_level_main_yaml() {
+        let manifest_directory = PathBuf::from("/tmp");
+        let location = PathBuf::from("/tmp/main.yaml");
+
+        assert_eq!("main", get_manifest_name(&manifest_directory, &location));
+    }
+
+    #[test]
+    fn test_main_yaml() {
+        let manifest_directory = PathBuf::from("/tmp");
+        let location = PathBuf::from("/tmp/test/main.yaml");
+
+        assert_eq!("test", get_manifest_name(&manifest_directory, &location));
+    }
+
+    #[test]
+    fn test_main_yml() {
+        let manifest_directory = PathBuf::from("/tmp");
+        let location = PathBuf::from("/tmp/test/main.yml");
+
+        assert_eq!("test", get_manifest_name(&manifest_directory, &location));
+    }
+
+    #[test]
+    fn test_non_main_yaml() {
+        let manifest_directory = PathBuf::from("/tmp");
+        let location = PathBuf::from("/tmp/test/hello.yaml");
+
+        assert_eq!(
+            "test.hello",
+            get_manifest_name(&manifest_directory, &location)
+        );
+    }
+
+    #[test]
+    fn test_main_nested_yaml() {
+        let manifest_directory = PathBuf::from("/tmp");
+        let location = PathBuf::from("/tmp/test/nested/main.yaml");
+
+        assert_eq!(
+            "test.nested",
+            get_manifest_name(&manifest_directory, &location)
+        );
+    }
+
+    #[test]
+    fn test_non_main_nested_yaml() {
+        let manifest_directory = PathBuf::from("/tmp");
+        let location = PathBuf::from("/tmp/test/nested/hello.yaml");
+
+        assert_eq!(
+            "test.nested.hello",
+            get_manifest_name(&manifest_directory, &location)
+        );
+    }
+}
+
+#[cfg(test)]
+#[cfg(windows)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_main_yaml() {
+        let manifest_directory = PathBuf::from("C:\\");
+        let location = PathBuf::from("C:\\test\\main.yaml");
+
+        assert_eq!("test", get_manifest_name(&manifest_directory, &location));
+    }
+
+    #[test]
+    fn test_main_yml() {
+        let manifest_directory = PathBuf::from("C:\\");
+        let location = PathBuf::from("C:\\test\\main.yml");
+
+        assert_eq!("test", get_manifest_name(&manifest_directory, &location));
+    }
+
+    #[test]
+    fn test_non_main_yaml() {
+        let manifest_directory = PathBuf::from("C:\\");
+        let location = PathBuf::from("C:\\test\\hello.yaml");
+
+        assert_eq!(
+            "test.hello",
+            get_manifest_name(&manifest_directory, &location)
+        );
+    }
+
+    #[test]
+    fn test_main_nested_yaml() {
+        let manifest_directory = PathBuf::from("C:\\");
+        let location = PathBuf::from("C:\\test\\nested\\main.yaml");
+
+        assert_eq!(
+            "test.nested",
+            get_manifest_name(&manifest_directory, &location)
+        );
+    }
+
+    #[test]
+    fn test_non_main_nested_yaml() {
+        let manifest_directory = PathBuf::from("C:\\");
+        let location = PathBuf::from("C:\\test\\nested\\hello.yaml");
+
+        assert_eq!(
+            "test.nested.hello",
+            get_manifest_name(&manifest_directory, &location)
+        );
+    }
+}
