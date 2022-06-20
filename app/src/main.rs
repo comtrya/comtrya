b522b2449859e1f36c38905d8d1329dfa7280eb1
David Flanagan <david@rawkode.dev>
2022-06-20 16:46:19 +0100

    fix: remove debug statement

--- app/src/main.rs
+++ app/src/main.rs
@@ -56,8 +56,6 @@ fn configure_subscriber(args: &GlobalArgs) -> impl Subscriber {
         .with_target(false)
         .without_time();
 
-    println!("Args: {}", args.verbose);
-
     match args.verbose {
         0 => builder,
         1 => builder.with_max_level(Level::DEBUG),
