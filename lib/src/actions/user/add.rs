f86ef341fd73cbfcc49b1b4b2bbdd5bbe7cc3a93
martintc <kj4ntv@gmail.com>
2022-07-03 20:33:44 -0700

    Initial implementaiton of user management targeting FreeBSD

---
+++ lib/src/actions/user/add.rs
@@ -0,0 +1,33 @@
+use super::User;
+use super::UserVariant;
+use crate::actions::Action;
+use crate::contexts::Contexts;
+use crate::manifests::Manifest;
+use crate::steps::Step;
+use std::ops::Deref;
+use tracing::{error, span};
+
+pub type UserAdd = User;
+
+impl Action for UserAdd {
+    fn plan(&self, _manifest: &Manifest, _context: &Contexts) -> Vec<Step> {
+        let variant: UserVariant = self.into();
+        let box_provider = variant.provider.clone().get_provider();
+        let provider = box_provider.deref();
+
+        let span = span!(
+            tracing::Level::INFO,
+            "user.add",
+            // provider = provider.name()
+        )
+        .entered();
+
+        let mut atoms: Vec<Step> = vec![];
+
+        atoms.append(&mut provider.add_user(&variant));
+
+        span.exit();
+
+        atoms
+    }
+}
