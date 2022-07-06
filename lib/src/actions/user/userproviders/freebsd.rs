f32e794de571be0885d9d8da00c57a8b978ab30b
martintc <kj4ntv@gmail.com>
2022-07-05 20:22:25 -0700

    Apply fixes for clippy warnings

--- lib/src/actions/user/userproviders/freebsd.rs
+++ lib/src/actions/user/userproviders/freebsd.rs
@@ -1,24 +1,18 @@
 use super::UserProvider;
-use crate::steps::finalizers::FlowControl::StopIf;
-use crate::steps::finalizers::OutputContains;
 use crate::steps::Step;
 use crate::{actions::user::UserVariant, atoms::command::Exec};
 use serde::{Deserialize, Serialize};
-use tracing::{instrument, warn};
-use which::which;
-// use os_info;
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct FreeBSDUserProvider {}
 
 impl UserProvider for FreeBSDUserProvider {
     fn add_user(&self, user: &UserVariant) -> Vec<Step> {
-        let mut steps: Vec<Step> = vec![];
         let mut args: Vec<String> = vec![];
 
         // is a user name isn't provided, cant create a new user
         if user.username.is_empty() {
-            return steps;
+            return vec![];
         }
 
         args.push(String::from("-n"));
@@ -27,7 +21,7 @@ impl UserProvider for FreeBSDUserProvider {
         if !user.home_dir.is_empty() {
             args.push(String::from("-m"));
             args.push(String::from("-d"));
-            args.push(String::from(user.home_dir.clone()));
+	    args.push(user.home_dir.clone());
         }
 
         if !user.shell.is_empty() {
@@ -37,7 +31,7 @@ impl UserProvider for FreeBSDUserProvider {
 
         if !user.fullname.is_empty() {
             args.push(String::from("-c"));
-            args.push(String::from(user.fullname.clone()));
+	    args.push(user.fullname.clone());
         }
 
         // default to setting a randomly generated password
