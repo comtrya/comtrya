ddbcbae8de2ec70241aa1a5b58d1e38df308529b
Todd Martin <todd@vultr.guest>
2022-07-15 04:48:08 +0000

    move to a new folder
    NoneUserProvider also changed implementation as the testing
    implementation was forgotten about, with now a proper NoneUserProvider
    implementation added

---
+++ lib/src/actions/user/providers/none.rs
@@ -0,0 +1,15 @@
+use super::UserProvider;
+use crate::steps::Step;
+use crate::{actions::user::UserVariant};
+use serde::{Deserialize, Serialize};
+use tracing::warn;
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct NoneUserProvider {}
+
+impl UserProvider for NoneUserProvider {
+    fn add_user(&self, _user: &UserVariant) -> Vec<Step> {
+	warn!("This system does not have a provider for users");
+	vec![]
+    }
+}
