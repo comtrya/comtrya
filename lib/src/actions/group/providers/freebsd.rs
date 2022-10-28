0f8331b298500f24508a85df6130600e75aa2593
Todd Martin <33560895+martintc@users.noreply.github.com>
2022-10-27 19:52:51 -0700

    [Feature] Freebsd user management (#258)
    * Initial code for FreeBSD group provider
    
    * Change add_group method
    
    * Correct compiler target
    
    * chain in group name
    
    * chain in group names
    
    * append group name to vector
    
    * remove which import
    
    * Initial implementation for freebsd add users to group
    
    * correct import in test
    
    * Correct the user provider being used
    
    * Implement adding users to groups during user creation step
    
    * fix formatting
    
    * space added as per cargo fmt
    
    * format fixes
    
    * Correct example by changing username in user.group action

---
+++ lib/src/actions/group/providers/freebsd.rs
@@ -0,0 +1,52 @@
+use super::GroupProvider;
+use crate::steps::Step;
+use crate::{actions::group::GroupVariant, atoms::command::Exec};
+use serde::{Deserialize, Serialize};
+use tracing::warn;
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct FreeBSDGroupProvider {}
+
+impl GroupProvider for FreeBSDGroupProvider {
+    fn add_group(&self, group: &GroupVariant) -> Vec<Step> {
+        if group.group_name.is_empty() {
+            warn!(message = "Unable to create group without a group name");
+            return vec![];
+        }
+
+        vec![Step {
+            atom: Box::new(Exec {
+                command: String::from("/usr/bin/pw"),
+                arguments: vec![String::from("groupadd"), group.group_name.clone()],
+                privileged: true,
+                ..Default::default()
+            }),
+            initializers: vec![],
+            finalizers: vec![],
+        }]
+    }
+}
+
+#[cfg(target_os = "freebsd")]
+#[cfg(tests)]
+mod test {
+    use crate::actions::group::*;
+
+    #[test]
+    fn test_add_group() {
+        let group_provider = FreeBSDGroupProvider {};
+        let steps = user_provider.add_user(&GroupVariant { group_name: test });
+
+        assert_eq!(steps.len(), 1);
+    }
+
+    #[test]
+    fn test_add_group_no_group_name() {
+        let group_provider = FreeBSDGroupProvider {};
+        let steps = group_provider.add_user(&GroupVariant {
+            // empty for test purposes
+        });
+
+        assert_eq!(steps.len(), 0);
+    }
+}
--- lib/src/actions/user/providers/freebsd.rs
+++ lib/src/actions/user/providers/freebsd.rs
@@ -40,7 +40,7 @@ impl UserProvider for FreeBSDUserProvider {
         args.push(String::from("-w"));
         args.push(String::from("random"));
 
-        vec![Step {
+        let mut steps: Vec<Step> = vec![Step {
             atom: Box::new(Exec {
                 command: String::from("/usr/sbin/pw"),
                 arguments: vec![String::from("useradd")]
@@ -52,11 +52,118 @@ impl UserProvider for FreeBSDUserProvider {
             }),
             initializers: vec![],
             finalizers: vec![],
-        }]
+        }];
+
+        if !user.group.is_empty() {
+            let user_groups = UserAddGroup {
+                username: user.username.clone(),
+                group: user.group.clone(),
+                provider: user.provider.clone(),
+            };
+            for group in self.add_to_group(&user_groups) {
+                steps.push(group);
+            }
+        }
+
+        steps
     }
 
-    fn add_to_group(&self, _user: &UserAddGroup) -> Vec<Step> {
-        warn!(message = "Functionality not implemented for platform");
-        vec![]
+    fn add_to_group(&self, user: &UserAddGroup) -> Vec<Step> {
+        let mut steps: Vec<Step> = vec![];
+
+        if user.group.is_empty() {
+            warn!(message = "No Groups listed to add user to");
+            return steps;
+        }
+
+        if user.username.is_empty() {
+            warn!(message = "No user specified to add to group(s)");
+            return steps;
+        }
+
+        for group in user.group.iter() {
+            steps.push(Step {
+                atom: Box::new(Exec {
+                    command: String::from("/usr/sbin/pw"),
+                    // arguments: vec![String::from("-a"), String::from("-G"), String::from(group)],
+                    arguments: vec![
+                        String::from("usermod"),
+                        user.username.clone(),
+                        String::from("-G"),
+                        String::from(group),
+                    ],
+                    privileged: true,
+                    ..Default::default()
+                }),
+                initializers: vec![],
+                finalizers: vec![],
+            });
+        }
+
+        steps
+    }
+}
+
+#[cfg(target_os = "freebsd")]
+#[cfg(test)]
+mod test {
+    use crate::actions::user::providers::{FreeBSDUserProvider, UserProvider};
+    use crate::actions::user::{add_group::UserAddGroup, UserVariant};
+
+    #[test]
+    fn test_add_user() {
+        let user_provider = FreeBSDUserProvider {};
+        let steps = user_provider.add_user(&UserVariant {
+            username: String::from("test"),
+            shell: String::from("sh"),
+            home_dir: String::from("/home/test"),
+            fullname: String::from("Test User"),
+            group: vec![],
+            ..Default::default()
+        });
+
+        assert_eq!(steps.len(), 1);
+    }
+
+    #[test]
+    fn test_add_user_no_username() {
+        let user_provider = FreeBSDUserProvider {};
+        let steps = user_provider.add_user(&UserVariant {
+            username: String::from(""),
+            shell: String::from("sh"),
+            home_dir: String::from("/home/test"),
+            fullname: String::from("Test User"),
+            group: vec![],
+            ..Default::default()
+        });
+
+        assert_eq!(steps.len(), 0);
+    }
+
+    #[test]
+    fn test_add_to_group() {
+        let user_provider = FreeBSDUserProvider {};
+        let steps = user_provider.add_to_group(&UserAddGroup {
+            username: String::from("test"),
+            group: vec![String::from("testgroup"), String::from("wheel")],
+            ..Default::default()
+        });
+
+        assert_eq!(steps.len(), 2);
+    }
+
+    #[test]
+    fn test_create_user_add_to_group() {
+        let user_provider = FreeBSDUserProvider {};
+        let steps = user_provider.add_user(&UserVariant {
+            username: String::from("test"),
+            shell: String::from(""),
+            home_dir: String::from(""),
+            fullname: String::from(""),
+            group: vec![String::from("testgroup")],
+            ..Default::default()
+        });
+
+        assert_eq!(steps.len(), 2);
     }
 }
