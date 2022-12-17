8a8aa0d06931faf19da1284f8ed9843fad47a856
Todd Martin <kj4ntv@gmail.com>
2022-12-16 21:35:02 -0800

    Remove use of unwraps

--- lib/src/manifests/load.rs
+++ lib/src/manifests/load.rs
@@ -23,88 +23,86 @@ pub fn load(manifest_path: PathBuf, contexts: &Contexts) -> HashMap<String, Mani
         .max_depth(Some(9))
         .build()
         // Don't walk directories
-        .filter(|entry| !entry.clone().unwrap().metadata().unwrap().is_dir())
         .filter(|entry| {
-            // There has to be a better way to do this.
-            // I couldn't get the TypeBuilder to work
+            !entry
+                .as_ref()
+                .ok()
+                .and_then(|entry| entry.metadata().ok().and_then(|entry| Some(entry.is_dir())))
+                .unwrap_or(false)
+        })
+        .filter(|entry| {
             entry
-                .clone()
-                .unwrap()
-                .file_name()
-                .to_str()
-                .unwrap()
-                .ends_with(".yaml")
-                || entry
-                    .clone()
-                    .unwrap()
-                    .file_name()
-                    .to_str()
-                    .unwrap()
-                    .ends_with(".yml")
+                .as_ref()
+                .ok()
+                .and_then(|entry| entry.file_name().to_str())
+                .map(|file_name| file_name.ends_with(".yaml") || file_name.ends_with(".yml"))
+                .unwrap_or(false)
         })
         // Don't consider anything in a `files` directory a manifest
         .filter(|entry| {
             !entry
-                .clone()
-                .unwrap()
-                .path()
-                .parent()
-                .unwrap()
-                .file_name()
-                .unwrap()
-                .eq("files")
+                .as_ref()
+                .ok()
+                .and_then(|entry| {
+                    entry.path().parent().and_then(|parent| {
+                        parent
+                            .file_name()
+                            .and_then(|file_name| Some(file_name.eq("files")))
+                    })
+                })
+                .unwrap_or(false)
         })
         .for_each(|entry| {
-            let filename = entry.unwrap();
+            if let Ok(filename) = entry {
+                let span = span!(
+                    tracing::Level::INFO,
+                    "manifest_load",
+                    manifest = filename.file_name().to_str()
+                )
+                .entered();
 
-            let span = span!(
-                tracing::Level::INFO,
-                "manifest_load",
-                manifest = filename.file_name().to_str().unwrap()
-            )
-            .entered();
+                let entry = canonicalize(filename.into_path()).unwrap();
+                let contents = std::fs::read_to_string(entry.clone()).unwrap_or(String::from(""));
+                let template = contents.as_str();
 
-            let entry = canonicalize(filename.into_path()).unwrap();
-            let contents = std::fs::read_to_string(entry.clone()).unwrap();
-            let template = contents.as_str();
+                let mut tera = Tera::default();
+                register_functions(&mut tera);
 
-            let mut tera = Tera::default();
-            register_functions(&mut tera);
+                let yaml = match tera.render_str(template, &to_tera(contexts)) {
+                    Ok(template) => template,
+                    Err(err) => {
+                        match err.source() {
+                            Some(err) => error!(message = err.source()),
+                            None => error!(message = err.to_string().as_str()),
+                        }
 
-            let yaml = match tera.render_str(template, &to_tera(contexts)) {
-                Ok(template) => template,
-                Err(err) => {
-                    match err.source() {
-                        Some(err) => error!(message = err.source()),
-                        None => error!(message = err.to_string().as_str()),
+                        span.exit();
+
+                        return;
                     }
+                };
 
-                    span.exit();
+                let mut manifest: Manifest = match serde_yaml::from_str(yaml.deref()) {
+                    Ok(manifest) => manifest,
+                    Err(e) => {
+                        error!(message = e.to_string().as_str());
+                        span.exit();
 
-                    return;
-                }
-            };
+                        return;
+                    }
+                };
 
-            let mut manifest: Manifest = match serde_yaml::from_str(yaml.deref()) {
-                Ok(manifest) => manifest,
-                Err(e) => {
-                    error!(message = e.to_string().as_str());
-                    span.exit();
+                let name =
+                    get_manifest_name(&manifest_path, &entry).expect("Failed to get manifest name");
 
-                    return;
-                }
-            };
+                manifest.root_dir = entry.parent().map(|parent| parent.to_path_buf());
 
-            let name =
-                get_manifest_name(&manifest_path, &entry).expect("Failed to get manifest name");
+                manifest.name = Some(name.clone());
 
-            manifest.root_dir = entry.parent().map(|parent| parent.to_path_buf());
+                manifests.insert(name, manifest);
 
-            manifest.name = Some(name.clone());
-
-            manifests.insert(name, manifest);
-
-            span.exit();
+                span.exit();
+            }
         });
 
     manifests
