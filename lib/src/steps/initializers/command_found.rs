16336ce75895ed6a908f6c3b735402cf149060c1
Todd Martin <33560895+martintc@users.noreply.github.com>
2022-12-18 19:27:06 -0800

    Windows directory copy (#292)
    * windows doesn't have "ls" on the path so make a test for windows that tests for a command in the path
    
    * implement windows specific implementation
    
    * remove dead code
    
    * fix formatting
    
    * Add a test for xcopy on windows

--- lib/src/steps/initializers/command_found.rs
+++ lib/src/steps/initializers/command_found.rs
@@ -32,6 +32,16 @@ mod tests {
         assert_eq!(true, result.is_ok());
         assert_eq!(true, result.unwrap());
     }
+    
+    #[cfg(target_family = "windows")]
+    #[test]
+    fn return_true_windows_xcopy() {
+        let initializer = CommandFound("Xcopy");
+        let result = initializer.initialize();
+
+        assert_eq!(true, result.is_ok());
+        assert_eq!(true, result.unwrap());
+    }
 
     #[cfg(target_family = "unix")]
     #[test]
