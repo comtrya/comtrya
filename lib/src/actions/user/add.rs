b06e17223ae6aabb756aea52dbcea0ef130c9d48
Yonas Yanfa <yonas.y@gmail.com>
2024-09-01 23:07:47 -0400

    Detect if users already exists instead of failing. Fixes #431 (#453)
    * Detect if users already exists instead of failing.
    
    * Add conditional OS check. uzers module only works on Unix systems.
    
    ---------
    
    Co-authored-by: yonas <yonas@build.lan>

--- lib/src/actions/user/add.rs
+++ lib/src/actions/user/add.rs
@@ -6,6 +6,9 @@ use crate::manifests::Manifest;
 use crate::steps::Step;
 use std::ops::Deref;
 
+#[cfg(unix)]
+use tracing::debug;
+
 pub type UserAdd = User;
 
 impl Action for UserAdd {
@@ -16,6 +19,17 @@ impl Action for UserAdd {
 
         let mut atoms: Vec<Step> = vec![];
 
+        if variant.username.is_empty() {
+            return Ok(atoms);
+        }
+
+        #[cfg(unix)]
+        match uzers::get_user_by_name(&variant.username) {
+            Some(_user) => debug!(message = "User already exists", username = ?variant.username),
+            None => atoms.append(&mut provider.add_user(&variant)?),
+        }
+
+        #[cfg(not(unix))]
         atoms.append(&mut provider.add_user(&variant)?);
 
         Ok(atoms)
